990ea2b5539d352ca6685372677484a600fd6cdd
fiftysev <nesgorai@bk.ru>
2021-04-10 20:49:18 +1000

    add hash

--- algAndStruct/215/215.cpp
+++ algAndStruct/215/215.cpp
@@ -1,54 +1,35 @@
 #include <string>
-#include <vector>
 #include <iostream>
-#include <sstream>
-#include <algorithm>
-#include <fstream>
+#include <vector>
 using namespace std;
-
-ifstream in ("input.txt");
-ofstream out ("output.txt");
-
-
-
-bool cmp (pair<string, string> a, pair<string, string> b) {
-    return a.first.length() > b.first.length();
-}
-
-
-void processFunction() {
-    int n, r;
-    string temp;
-    in >> r >> n;
-    getline(in, temp);
-    vector< pair <string, string> > replaceElements(r);
-    for (int i = 0; i < r; i ++)
-    {
-        getline(in, replaceElements[i].first);
-        getline(in, replaceElements[i].second);
-    }
-    sort(replaceElements.begin(), replaceElements.end(), cmp);
-    for (int i = 0; i < n; i++) {
-        string line;
-        getline(in, line);
-        vector <string> text(line.length() + 1);
-        text[0] = "";
-        for (int j = 1; j < line.length() + 1; j++) {
-            text[j] = text[j - 1] + line[j - 1];
-
-            for (auto element : replaceElements)
-            {
-                int replaceLen = element.first.size();
-                if (replaceLen <= j && line.compare(j - replaceLen, replaceLen, element.first) == 0 && text[j - replaceLen].length() < text[j].length())
-                    text[j] = text[j - replaceLen] + element.second;
-            }     
-        }
-        out << text[line.length()] << endl;
-    }
-}
 
 int main()
 {
-    processFunction();
-    return 0;
+    const int base = 6;
+    string str;
+    bool is_neg;
+    getline(cin, str);
+    vector<int> digits;
+    if (str.length() < 7)
+    {
+        cout << str;
+        return 0;
+    }
+    if (str[0] == '-')
+    {
+        is_neg = true;
+        str.erase(0, 1);
+    }
+    for (int i = 0; i < str.length(); i += base)
+    {
+        digits.push_back(atoi(str.substr(i, 6).c_str()));
+    }
+    string result;
+    if (is_neg)
+        cout << '-';
+    for (int i = 0; i < digits.size(); i++)
+    {
+        cout << digits[i];
+    }
+    cout << result;
 }
