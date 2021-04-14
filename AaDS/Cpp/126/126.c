d844ba0b9ffba3ca18750fe8db32ef79a22df4c2
RedDreadThomas <49274206+RedDreadThomas@users.noreply.github.com>
2021-04-14 11:13:20 +1000

    Delete AaDS directory

--- AaDS/Cpp/126/126.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int user_input;
-    int zero_index;
-    scanf("%d %d", &user_input, &zero_index);
-    int mask = ~(1 << zero_index);
-    printf("%d", user_input & mask);
-}
