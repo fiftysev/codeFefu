9bb8b0fdc96f576913fe5ad5534a4650dbfa59de
RedDreadThomas <reddreadthomaas@yandex.ru>
2021-04-18 23:07:05 +1000

    All necessary tasks with long numbers were solved

--- strWatchersWorks/AaDS/Cpp/LongNumbers/main.cpp
+++ strWatchersWorks/AaDS/Cpp/LongNumbers/main.cpp
@@ -1,32 +1,140 @@
 #include <bits/stdc++.h>
+
+#include <utility>
+
 using namespace std;
 
-const int base = 100000;
-const int chunk_len = 5;
+const int base = 10000;
+const int chunk_len = 4;
 
+string chunk_to_string(int);
 
-vector <int> create_long_num(string goal) {
-    vector <int> ln;
-    int sign;
-    if (goal[0] == '-') {
-        sign = 1;
-        goal.replace(0, 1, "");
-    }
-    else {
-        sign = 0;
-    }
+struct ln {
+    vector <int> value;
+    bool sign;
 
-    while (goal.length() > chunk_len) {
-        ln.push_back(stoi(goal.substr(goal.length() - chunk_len, chunk_len)));
-        goal.replace(goal.length() - chunk_len, chunk_len, "");
+    explicit ln () {
+        value = vector<int>();
+        sign = false;
+
     }
 
-    ln.push_back(stoi(goal));
-    ln.push_back(sign);
-    return ln;
-}
+    explicit ln (string goal) {
+        if (goal[0] == '-') {
+            sign = true;
+            goal.replace(0, 1, "");
+        }
+        else {
+            sign = false;
+        }
+        value = vector <int>();
+        while (goal.length() > chunk_len) {
+            value.push_back(stoi(goal.substr(goal.length() - chunk_len, chunk_len)));
+            goal.replace(goal.length() - chunk_len, chunk_len, "");
+        }
+        value.push_back(stoi(goal));
+    }
 
-string chunktostr(int chunk) {
+    explicit ln (vector <int> value, bool sign) {
+        this->value = move(value);
+        this->sign = sign;
+    }
+
+    explicit ln(int value) {
+        string goal = to_string(value);
+        ln result = ln(goal);
+        this->value = result.value;
+        this->sign = result.sign;
+    }
+
+    void print() {
+        vector <int>::iterator ln_it;
+
+        if (sign)
+            cout << "-";
+
+        cout <<  *(value.end() - 1);
+
+        for (ln_it = value.end() - 2; ln_it >= value.begin(); --ln_it) {
+            int chunk = *ln_it;
+            cout << chunk_to_string(chunk);
+        }
+
+        cout << endl;
+    }
+
+    ln shift(int rad_count) {
+        ln result = ln();
+        for (int i = 0; i < rad_count; ++i) {
+            result.value.push_back(0);
+        }
+        for (int num: this->value) {
+            result.value.push_back(num);
+        }
+        return result;
+    }
+
+    ln sum(ln b) {
+        if (this->value.size() < b.value.size()) {
+            vector <int> temp = this->value;
+            this->value = b.value;
+            b.value = temp;
+        }
+        vector <int> result_value;
+        int cur_result = 0;
+        for (int i = 0; i < b.value.size(); ++i) {
+            cur_result += this->value[i] + b.value[i];
+            result_value.push_back(cur_result % base);
+            cur_result /= base;
+        }
+
+        for (int i = b.value.size(); i < this->value.size(); ++i) {
+            cur_result += this->value[i];
+            result_value.push_back(cur_result % base);
+            cur_result /= base;
+        }
+        if (cur_result != 0) {
+            result_value.push_back(cur_result);
+        }
+
+        ln result = ln(result_value, false);
+        return result;
+    }
+
+    ln multiple_with_int(int b) {
+        int cur_result = 0;
+        vector <int> result_value;
+        bool b_s = b < 0;
+        bool result_sign = this->sign && b_s;
+
+        if (b_s) b *= -1;
+
+        for (int i : this->value) {
+            cur_result += i * b;
+            result_value.push_back(cur_result % base);
+            cur_result /= base;
+        }
+        if (cur_result) {
+            result_value.push_back(cur_result);
+        }
+
+        ln result = ln(result_value, result_sign);
+        return result;
+    }
+
+
+    ln multiple_with_ln(const ln& b) {
+        ln result = ln(0);
+        for (int i = 0; i < b.value.size(); ++i) {
+            result = result.sum(this->multiple_with_int(b.value[i]).shift(i));
+        }
+        return result;
+    }
+
+
+};
+
+string chunk_to_string(int chunk) {
     string result;
     int m = base / 10;
     while (m > 0) {
@@ -35,54 +143,14 @@ string chunktostr(int chunk) {
         m /= 10;
     }
     return result;
-}
-
-void print_long_num(vector <int> ln) {
-    vector <int>::iterator ln_it;
-
-    if (ln.back() == 1)
-        cout << "-";
-
-    cout <<  *(ln.end() - 2);
-
-    for (ln_it = ln.end() - 3; ln_it >= ln.begin(); --ln_it) {
-        int chunk = *ln_it;
-        cout << chunktostr(chunk);
-    }
-
-    cout << endl;
-
 
 }
 
-vector <int> sum(vector <int> a, vector <int> b) {
-
-    if (a.size() < b.size()) {
-        vector <int> temp = a;
-        a = b;
-        b = temp;
+ln factorial(int a) {
+    ln result = ln(1);
+    for (int i = 2; i <= a; ++i) {
+        result = result.multiple_with_int(i);
     }
-    vector <int> result;
-
-    int cur_result = 0;
-
-    for (int i = 0; i < b.size() - 1; ++i) {
-        cur_result += a[i] + b[i];
-        result.push_back(cur_result % base);
-        cur_result /= base;
-    }
-
-    for (int i = b.size() - 1; i < a.size() - 1; ++i) {
-        cur_result += a[i];
-        result.push_back(cur_result % base);
-        cur_result /= base;
-    }
-
-    if (cur_result != 0) {
-        result.push_back(cur_result);
-    }
-
-    result.push_back(a.back());
     return result;
 }
 
@@ -90,11 +158,7 @@ int main() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 
-    string l1, l2;
-    cin >> l1 >> l2;
-    vector <int> ln1 = create_long_num(l1);
-    vector <int> ln2 = create_long_num(l2);
-    print_long_num(sum(ln1, ln2));
-
+    int t1;
+    cin >> t1;
+    factorial(t1).print();
 }
-
